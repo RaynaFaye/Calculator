bd097d84f8c67b17bcadf224cb96ab269d75ada9
Serena <serenafayelombardo@gmail.com>
2020-01-09 10:27:47 +0100

    Add action key and equal action key in works with two numbers

--- js/main.js
+++ js/main.js
@@ -1,30 +1,51 @@
 const calculatorButtons = document.querySelector('.calculator__buttons');
 const result = document.querySelector('.calculator__current-value');
+const previousEntry = document.querySelector('.calculator__previous-value');
 let value = '';
+let numberEntries = [];
+let actionCalled = '';
+
+function add(num1, num2) {
+  return Number(num1) + Number(num2);
+}
 
 //What to do when clicking on buttons
 calculatorButtons.addEventListener('click', function(event) {
   let currentNumber = event.target.textContent;
   let actionKey = event.target.dataset.action;
   if (!actionKey) {
-    console.log('number key');
     value = value + currentNumber;
     //Show the current result on top of the calculator
     result.textContent = value;
   }
+  //Action keys (+ - * /)
   if (actionKey === 'add' || actionKey === 'substract' || actionKey === 'multiple' || actionKey === 'divide') {
-    console.log('operator key');
+    if (actionKey === 'add') {
+      actionCalled = 'add';
+      numberEntries.push(value);
+      previousEntry.textContent = `${value} + `;
+      value = '';
+    }
   }
+  //Equal Key : Currently just simple with only two numbers
   if (actionKey === 'equal') {
-    console.log('equal key');
+    if (actionCalled === 'add') {
+      numberEntries.push(value);
+      let finalResult = add(...numberEntries);
+      result.textContent = finalResult;
+      previousEntry.textContent = `${previousEntry.textContent} ${value} = ${finalResult} `;
+      value = '';
+      numberEntries = [];
+    }
   }
+  //Clear everything
   if (actionKey === 'clear') {
-    console.log('clear key');
+    previousEntry.textContent = 0;
     result.textContent = 0;
     value = '';
   }
+  //Take of last value on current shown result
   if (actionKey === 'back') {
-    console.log('return key');
     result.textContent = result.textContent.slice(0, -1);
     value = result.textContent;
   }
