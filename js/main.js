409240131d371d15f3958815d46fef213d2e7ed9
Serena <serenafayelombardo@gmail.com>
2020-01-09 09:08:46 +0100

    Current number typed shown on interface and clear key clears it

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
 const calculatorButtons = document.querySelector('.calculator__buttons');
+const result = document.querySelector('.calculator__result');
+let value = '';
+
+//What to do when clicking on buttons
 calculatorButtons.addEventListener('click', function(event) {
   let currentNumber = event.target.textContent;
-  console.log(currentNumber);
   let actionKey = event.target.dataset.action;
   if (!actionKey) {
     console.log('number key');
+    value = value + currentNumber;
+    //Show the current result on top of the calculator
+    result.textContent = value;
   }
   if (actionKey === 'add' || actionKey === 'substract' || actionKey === 'multiple' || actionKey === 'divide') {
     console.log('operator key');
@@ -14,12 +20,15 @@ calculatorButtons.addEventListener('click', function(event) {
   }
   if (actionKey === 'clear') {
     console.log('clear key');
+    result.textContent = 0;
+    value = '';
   }
   if (actionKey === 'back') {
     console.log('return key');
   }
 });
 
-document.addEventListener('keydown', function(event) {
-  console.log(event.key);
-});
+//Get value on keypress not currently set up but kept for the time being
+// document.addEventListener('keydown', function(event) {
+//   console.log(event.key);
+// });
